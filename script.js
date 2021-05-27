5fc6dab3290f6fbef2d5ae7d77f4c1aacbd79fd6
Lucas Ciannella <lucasciannella@hotmail.com>
2021-05-27 00:16:23 -0300

    atualizações 27/05
    subindo arquivo de audio, e atualizações do simulador

--- script.js
+++ script.js
@@ -37,14 +37,14 @@ function showSlides(n) {
 function checar() {
   const interacao = document.getElementById('int')
   const lamp = document.getElementById('lamp');     // lampada
-//check boxxes 
+  //check boxxes 
   const lampNeutral = document.getElementById('lampNeutral'); // fio neutro que vai na lampada
   const force = document.getElementById('force'); // fio fase 
   const lampReturn = document.getElementById('lampReturn'); // fio de retorno da lampada 
   const neutralForce = document.getElementById('neutralForce'); // fio neutro caixa de força 
   const forceCircuitBreaker = document.getElementById('forceCircuitBreaker') // fio do dijuntor 
   const returnCircuitBreaker = document.getElementById('returnCircuitBreaker') // retorno do dijuntor 
-//divs que simulam os fios 
+  //divs que simulam os fios 
   const fiofase = document.getElementById('fioFase') // div que faz o fio fase 
   const fioNeutro = document.getElementById('fioNeutro') // div que faz o fio neutro 
   const fioNeutroC = document.getElementById('fioNeutroC') // div fio neutro 
@@ -55,31 +55,33 @@ function checar() {
 
   if (force.checked && forceCircuitBreaker.checked) {
     fiofase.style = 'border-top: 4px solid black; border-left: 4px solid black; border-right: 4px solid black;'
-  }   else { 
+  } else {
     fiofase.style = ''
-  } if (neutralForce.checked && lampNeutral.checked){
+  } if (neutralForce.checked && lampNeutral.checked) {
     fioNeutro.style = 'border-left: 4px solid blue; border-top: 4px solid blue;'
-    fioNeutroC.style = ' border-right: 4px solid blue;'
+    fioNeutroC.style = ' border-right: 4px solid blue ;'
   } else {
     fioNeutroC.style = ''
     fioNeutro.style = ''
-  } if (lampReturn.checked && returnCircuitBreaker.checked){
+  } if (lampReturn.checked && returnCircuitBreaker.checked) {
     fioRetorno.style = 'border-top: 4px solid burlywood; border-right: 4px solid burlywood;'
     fioRetornoC.style = ' border-left: 4px solid burlywood;'
   } else {
     fioRetornoC.style = ''
     fioRetorno.style = ''
-  } if(force.checked && forceCircuitBreaker.checked && neutralForce.checked && lampNeutral.checked && lampReturn.checked && returnCircuitBreaker.checked){
+  } if (force.checked && forceCircuitBreaker.checked && neutralForce.checked && lampNeutral.checked && lampReturn.checked && returnCircuitBreaker.checked) {
     lamp.style.backgroundImage = 'url("acesa.png")'
     interruptor.style.backgroundImage = 'url("interruptorright.png")'
-    
-    interacao.innerHTML += '<div> PARABÉNS, você conseguiu </div> '
-    
+    interacao.innerHTML += '<div class"center"> Parabéns <br> Você conseguiu!</div> '
+    interacao.innerHTML += '<audio src="aplausos.mp3"> </audio>'
+    const audio = document.querySelector('audio')
+    audio.play()
 
-  } else{
-  lamp.style.backgroundImage = 'url("apagada.png")'
-  interruptor.style.backgroundImage = 'url("interruptorleft.png")'
-  interacao.innerText = ''
+
+  } else {
+    lamp.style.backgroundImage = 'url("apagada.png")'
+    interruptor.style.backgroundImage = 'url("interruptorleft.png")'
+    interacao.innerText = ''
   }
 
 
